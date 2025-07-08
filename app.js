35c5dac9220cd3630240fa3c94bebc588ce08a8f
Liokang <60290160+Liokang@users.noreply.github.com>
2025-07-08 11:52:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,27 +17,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 
 app.get("/", (req, res) => {
-  const projects = [
-    {
-      title: "Campground App",
-      description: "Created a database-backed web app with reviews, maps using Mapbox API, and Passport.js authentication.",
-      link: "https://github.com/Liokang/HelpYelp",
-      image: ["/images/campground/campground.jpg","/images/campground/1.jpg","/images/campground/2.jpg"]
-    },
-    {
-      title: "Street Vendor Booking App",
-      description: "Built vendor & customer booking system, integrated Mapbox, and dashboard functionalities.",
-      link: "https://github.com/Liokang/streetVendor2",
-      image: ["/images/streetvendor/1.jpg","/images/streetvendor/2.jpg","/images/streetvendor/3.png"]
-    },
-    {
-      title: "Movie Recommendation",
-      description: "Python & Node.js app recommending movies using similarity scores. Integrated PythonShell with Node.",
-      link: "https://github.com/Liokang/movieRecommendation",
-      image: ["/images/Movie Recommendation/movierecommendatino.PNG","/images/Movie Recommendation/1.PNG","/images/Movie Recommendation/3.PNG"]
-    }
-  ];
-  res.render("index", { projects });
+  
+  res.render("index");
 });
 
 app.listen(PORT, function(err){
